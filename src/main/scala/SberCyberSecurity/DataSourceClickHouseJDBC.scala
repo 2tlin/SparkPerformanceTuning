6bc3f4cfca67c555e19be1be25470667aae76051
dvatlin <dvatlin@mail.ru>
2021-11-25 18:02:23 +0300

    Corrected DataSourceClickHouseJDBC

--- src/main/scala/SberCyberSecurity/DataSourceClickHouseJDBC.scala
+++ src/main/scala/SberCyberSecurity/DataSourceClickHouseJDBC.scala
@@ -132,7 +132,7 @@ object DataSourceClickHouseJDBC {
 //    createTable2CH(properties, "Users")
 //    writeFile2CH("spark-warehouse/persons2", "Persons2")
 
-    // https://clickhouse.com/docs/ru/getting-started/example-datasets/menus/
+    // ttps://clickhouse.com/docs/ru/getting-started/example-datasets/menus/
     // use CH in docker from Docker HUB
     val tableMenu = "menu"
     val tableDish = "dish"
